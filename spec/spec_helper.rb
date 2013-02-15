e27209b1e7df81669488c8ad902c8b28ff65311d
Theo <theo@iconara.net>
2013-02-15 21:12:26 +0100

    Write .classpath from .rvmrc

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 # encoding: utf-8
 
+require 'java'
+
+IO.foreach(File.expand_path('../../.classpath', __FILE__)) { |path| $CLASSPATH << path.chomp }
+
+require 'bundler'; Bundler.setup(:default, :test)
+
+require 'tmpdir'
+
 require 'humboldt'
 require 'humboldt/emr_flow'
 require 'humboldt/hadoop_status_filter'
