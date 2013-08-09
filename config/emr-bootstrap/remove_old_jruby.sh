89bb03b9e520a2a3990088ebaaee72be8dd01550
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-08-09 10:08:06 +0200

    Only remove old JRuby jar if it's present

--- config/emr-bootstrap/remove_old_jruby.sh
+++ config/emr-bootstrap/remove_old_jruby.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-rm /home/hadoop/lib/jruby-complete-no-joda-1.6.5.jar
+if [ -e /home/hadoop/lib/jruby-complete-no-joda-1.6.5.jar ]
+  then
+    rm /home/hadoop/lib/jruby-complete-no-joda-1.6.5.jar
+fi
