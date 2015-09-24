c3f124a4967e16fa5f9e3b3535667f398c9d4958
Theo <theo@iconara.net>
2015-09-24 16:51:58 +0200

    Update the BinaryComparator unit tests to test the raw byte #compare too

--- spec/humboldt/binary_comparator_spec.rb
+++ spec/humboldt/binary_comparator_spec.rb
@@ -22,8 +22,47 @@ module Humboldt
         ]
       end
 
+      let :left_offset do
+        0
+      end
+
+      let :right_offset do
+        -1
+      end
+
+      def prefix_size(w)
+        case w
+        when Hadoop::Io::BytesWritable
+          4
+        when Hadoop::Io::Text
+          Hadoop::Io::WritableUtils.decodeVIntSize(writable_to_bytes(w)[0])
+        else
+          raise 'Not supported'
+        end
+      end
+
+      def writable_to_bytes(w)
+        Hadoop::Io::WritableUtils.to_byte_array(w)
+      end
+
+      def should_compare(k1, k2, assertion)
+        compare_writables(k1, k2).should(assertion)
+        compare_bytes(k1, k2).should(assertion)
+      end
+
+      def compare_writables(k1, k2)
+        comparator.compare(k1, k2)
+      end
+
+      def compare_bytes(k1, k2)
+        b1 = writable_to_bytes(k1)
+        b2 = writable_to_bytes(k2)
+        comparator.compare(b1, 0, k1.length + prefix_size(k1), b2, 0, k2.length + prefix_size(k1))
+      end
+
       before do
         described_class.set_offsets(conf, left_offset, right_offset)
+        conf.set(Hadoop::Mapreduce::MRJobConfig::MAP_OUTPUT_KEY_CLASS, keys.first.java_class.name)
         comparator.set_conf(conf)
       end
 
@@ -37,27 +76,27 @@ module Humboldt
         end
 
         it 'returns 0 when comparing a writable to itself' do
-          comparator.compare(keys[0], keys[0]).should eq(0)
+          should_compare(keys[0], keys[0], eq(0))
         end
 
         it 'returns -1 when the first argument is lesser than the second' do
-          comparator.compare(keys[0], keys[3]).should eq(-1)
+          should_compare(keys[0], keys[3], eq(-1))
         end
 
         it 'returns 1 when the first argument is greater than the second' do
-          comparator.compare(keys[3], keys[0]).should eq(1)
+          should_compare(keys[3], keys[0], eq(1))
         end
 
         it 'returns 0 when the specified slice of the arguments are equal' do
-          comparator.compare(keys[0], keys[1]).should eq(0)
+          should_compare(keys[0], keys[1], eq(0))
         end
 
         it 'returns -1 when the specified slice of the first argument is lesser than the same slice of second' do
-          comparator.compare(keys[0], keys[2]).should eq(-1)
+          should_compare(keys[0], keys[2], eq(-1))
         end
 
         it 'returns 1 when the specified slice of the first argument is greater than the same slice of the second' do
-          comparator.compare(keys[2], keys[0]).should eq(1)
+          should_compare(keys[2], keys[0], eq(1))
         end
       end
 
@@ -71,27 +110,27 @@ module Humboldt
         end
 
         it 'returns 0 when comparing a writable to itself' do
-          comparator.compare(keys[0], keys[0]).should eq(0)
+          should_compare(keys[0], keys[0], eq(0))
         end
 
         it 'returns -1 when the first argument is lesser than the second' do
-          comparator.compare(keys[0], keys[2]).should eq(-1)
+          should_compare(keys[0], keys[2], eq(-1))
         end
 
         it 'returns 1 when the first argument is greater than the second' do
-          comparator.compare(keys[2], keys[0]).should eq(1)
+          should_compare(keys[2], keys[0], eq(1))
         end
 
         it 'returns 0 when the specified slice of the arguments are equal' do
-          comparator.compare(keys[0], keys[3]).should eq(0)
+          should_compare(keys[0], keys[3], eq(0))
         end
 
         it 'returns -1 when the specified slice of the first argument is lesser than the same slice of second' do
-          comparator.compare(keys[0], keys[2]).should eq(-1)
+          should_compare(keys[0], keys[2], eq(-1))
         end
 
         it 'returns 1 when the specified slice of the first argument is greater than the same slice of the second' do
-          comparator.compare(keys[2], keys[0]).should eq(1)
+          should_compare(keys[2], keys[0], eq(1))
         end
       end
 
@@ -114,17 +153,34 @@ module Humboldt
         end
 
         it 'returns 0 when comparing a writable to itself' do
-          comparator.compare(keys[0], keys[0]).should eq(0)
+          should_compare(keys[0], keys[0], eq(0))
         end
 
         it 'counts the offsets from the end of the byte arrays' do
-          sorted_keys = keys.sort { |k1, k2| comparator.compare(k1, k2) }.map { |k| String.from_java_bytes(k.bytes) }
+          sorted_keys = keys.sort { |k1, k2| compare_writables(k1, k2) }.map { |k| String.from_java_bytes(k.bytes) }
           sorted_keys.should eq(%w[
             xxxx1xxx
             xx2xxx
             xxx3xxx
             x4xxx
           ])
+          sorted_keys = keys.sort { |k1, k2| compare_bytes(k1, k2) }.map { |k| String.from_java_bytes(k.bytes) }
+          sorted_keys.should eq(%w[
+            xxxx1xxx
+            xx2xxx
+            xxx3xxx
+            x4xxx
+          ])
+        end
+      end
+
+      context 'when the writable does not align with the start of the byte array' do
+        it 'correctly accounts for the offset' do
+          b1 = writable_to_bytes(Hadoop::Io::BytesWritable.new('foobar'.to_java_bytes))
+          bytes = String.from_java_bytes(b1)
+          bytes.prepend("\xff")
+          b2 = bytes.to_java_bytes
+          comparator.compare(b1, 0, 10, b2, 1, 10).should eq(0)
         end
       end
 
@@ -153,11 +209,13 @@ module Humboldt
         end
 
         it 'returns 0 when comparing a Text to itself' do
-          comparator.compare(keys[0], keys[0]).should eq(0)
+          should_compare(keys[0], keys[0], eq(0))
         end
 
         it 'counts the offsets from the end of the byte arrays' do
-          sorted_keys = keys.sort { |k1, k2| comparator.compare(k1, k2) }.map { |k| String.from_java_bytes(k.bytes) }
+          sorted_keys = keys.sort { |k1, k2| compare_writables(k1, k2) }.map { |k| String.from_java_bytes(k.bytes) }
+          sorted_keys.should eq(strings.values_at(3, 1, 0, 2))
+          sorted_keys = keys.sort { |k1, k2| compare_bytes(k1, k2) }.map { |k| String.from_java_bytes(k.bytes) }
           sorted_keys.should eq(strings.values_at(3, 1, 0, 2))
         end
       end
@@ -174,7 +232,7 @@ module Humboldt
         it 'raises an error' do
           i1 = Hadoop::Io::IntWritable.new(3)
           i2 = Hadoop::Io::IntWritable.new(4)
-          expect { comparator.compare(i1, i2) }.to raise_error(java.lang.ClassCastException)
+          expect { compare_writables(i1, i2) }.to raise_error(java.lang.ClassCastException)
         end
       end
     end
